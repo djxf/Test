b2cef1692f3c73d7aaac482ff24fa40f26394180
djxf <1763380121@1763380121@qq.com>
2019-05-29 22:17:36 +0800

    change 蜂鸟众包.js

--- fnzb.js
+++ fnzb.js
@@ -1,5 +1,21 @@
 auto("fast");
-//子线程点击确定
+
+//生成从minNum ~ maxNum的随机数
+function getrandomNum(minNum,maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
+}
+
+//子线程点击确定 同时查找单子
 threads.start(function(){
     while(true){
         var re_cancle = text(cancle).findOne();
@@ -7,30 +23,27 @@ threads.start(function(){
             click(ok);
           }else{
             log("未找到控件 但执行函数成功");
-            //toast("null");
         }
-        //click(ok);
-        //log(threads);
-        //log(ok);
         if(currentPackage()!=package_name){
             // toast(todo_exit);
-            //心碎;
         }else{
             log(currentPackage);
         }
          //查找须设置超时时间否则陷入死循环
-    var bt_helpBuy = text(helpbuy).findOne(200);
-    if(bt_helpBuy != null){
-        if(bt_helpBuy.text == "帮买"){
-            toast("已检测到帮买");
-            click("抢单");
-        }
-    }
+         var bt_helpBuy = id(qdId).findOne(250);
+         if(bt_helpBuy != null){
+                 toast("已检测到抢单");
+                 var trueC = bt_helpBuy.click();
+         }
+         if(trueC){
+            click(ok);
+         }
     }
 });
 
 var ok = "确认";
-var helpbuy = "帮买"
+var helpbuy = "抢单"    //新版本中抢单已经不是文字
+var qdId = "ia";        //抢单按钮的id
 var threads = "子线程";
 var main_thread = "主线程";
 var refresh = "刷新订单";
@@ -39,7 +52,7 @@ var todo = "成功打开蜂鸟众包平台 即将为你抢单"
 var todo_exit = "检测到已退出平台 已停止抢单 抢单请重新打开"
 var package_name = "me.ele.crowdsource";
 var cancle = "取消";
-var isOpen;
+var isOpen;             //记录打开蜂鸟是否成功
 auto("fast");
 isOpen = app.launchPackage(package_name);
 if(isOpen){
@@ -49,46 +62,27 @@ if(isOpen){
 //主线程
 while(true){
     //线程睡眠 延迟点击速度 防止被屏蔽
-    sleep(100);
+    var randomtime = getrandomNum(5,20);
+    toast("本次暂停的随机时间："+ randomtime);
+    sleep(randomtime*1000);
     click(refresh);
-
-    //查找须设置超时时间否则陷入死循环
-    var bt_helpBuy = text(helpbuy).findOne(250);
+    //查找须设置超时时间否则陷入死循环 id = ia
+    var bt_helpBuy = id(qdId).findOne(250);
     if(bt_helpBuy != null){
-        if(bt_helpBuy.text == "帮买"){
-            toast("已检测到帮买");
-            click("抢单");
-        }
+            device.vibrate(100);
+            toast("已检测到抢单");
+            bt_helpBuy.click();
     }
-   
-    
-    //抢单提醒
-    var block = text("抢单").findOne(250);
-  
-    if(block!=null){
-        device.vibrate(100);
-        sleep(60000);
-        toast("有单子啦 快来抢啊");
-        click("抢单");
-    }
-
-
-   
     if(currentPackage()!=package_name){
-        //toast(todo_exit);
         log(currentPackage());
-        //心碎;
-        //心碎会退出当前循环if不会阻止此函数执行。
+        //会退出当前循环if不会阻止此函数执行。
     }else{
         log(currentPackage());
     }
-    
     var re_cancle = text(cancle).findOne(100);
     if(re_cancle!=null){
             click(ok);
     }else{
             log("未找到控件 但执行函数成功");
-            //toast("null");
     }
-   // log(main_thread);
 }
